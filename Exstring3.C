6c81280f536c48a900f695a171669446410c98ce
Guilherme Roth Cassol <129892129+guicassol8@users.noreply.github.com>
2023-05-23 10:49:06 -0300

    Create Exstring3.C

---
+++ Exstring3.C
@@ -0,0 +1,21 @@
+#include "stdio.h"
+int	comprimento_string(char	str[]);
+int main(){
+    char string [32];
+    printf ("Digite uma palavra\n");
+    scanf ("%s",string);
+    int size = comprimento_string (string);
+    printf ("%d\n",size);
+    return 0;
+}
+int	comprimento_string(char	str[]){
+    int size=0;
+    for (int i=0;str[i]>0;i++)
+    {
+        if (str[i]=='a'||str[i]=='u'||str[i]=='o'||str[i]=='i'||str[i]=='e')
+        {
+        size ++;    
+        }
+    }
+    return size;
+}
