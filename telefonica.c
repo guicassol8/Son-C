8042837f49f39d24970ade1cc4b63a30547bc791
Guilherme Roth Cassol <129892129+guicassol8@users.noreply.github.com>
2023-05-25 11:15:06 -0300

    Create telefonica.c

---
+++ telefonica.c
@@ -0,0 +1,93 @@
+#include "stdio.h"
+#include "string.h"
+
+struct usuario
+{
+    long cpf;
+    char nome [20];
+    char sobrenome [20];
+};
+
+int totalUsuarios = 0;
+struct usuario usuarios [100];
+
+void listUsuario()
+{
+    for (int i=0;i < totalUsuarios; i++)
+    {
+        printf ("Nome: %s\n", usuarios [i].nome);
+        printf ("Sobrenome: %s\n", usuarios [i].sobrenome);
+        printf ("CPF: %ld\n", usuarios [i].cpf);
+    }
+}
+
+void creatUsuario(char nome[],char sobrenome[], long cpf)
+{
+    struct usuario u1;
+    strcpy (u1.nome,nome);
+    strcpy (u1.sobrenome,sobrenome);
+    u1.cpf = cpf;
+    usuarios[totalUsuarios] = u1;
+    totalUsuarios++;
+    printf ("Usuario Registrado.\n");
+}
+
+void updateUsuario ()
+{
+
+}
+
+void deleteUsuario()
+{
+    
+}
+
+int main ()
+{
+    char nome [20];
+    char sobrenome [20];
+    long cpf;
+    int opcao = 0;
+    while(1)
+    {
+        printf ("\nCentral Telefonica\n");
+        printf ("1 - Registrar Usuario\n");
+        printf ("2 - Listar Usuarios\n");
+        printf ("3 - Atualizar Usuario\n");
+        printf ("4 - Deletar Usuario\n");
+        printf ("5 - Sair\n");
+        
+        scanf ("%d",&opcao);
+
+    switch (opcao)
+    {
+    case 1:
+        
+        printf ("Nome: ");
+        scanf ("%s",nome);
+        
+        printf ("\nSobrenome: ");
+        scanf ("%s",sobrenome);
+
+        printf ("\nCPF: ");
+        scanf ("%ld",&cpf);
+
+        creatUsuario(nome,sobrenome,cpf);
+        break;
+
+    case 2:
+        listUsuario();
+        break;
+
+    case 3:
+        break;
+        
+    case 4:
+        break;
+
+    case 5:
+        break;
+    }
+    }
+    return 0;
+}
